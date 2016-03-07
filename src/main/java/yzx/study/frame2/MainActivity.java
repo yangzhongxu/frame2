4512bb90523aa8f1969b78b204e8aaa83e3f9b46
yangzhongxu <zhongxu.yang@aliyun.com>
2016-03-07 10:51:50 +0800

    first commit : EventBroadcast

--- src/main/java/yzx/study/frame2/MainActivity.java
+++ src/main/java/yzx/study/frame2/MainActivity.java
@@ -1,7 +1,12 @@
 package yzx.study.frame2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+import yzx.study.frame2.util.EventBroadcast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        EventBroadcast.init(this);
+        EventBroadcast.register(1, receiver);
+        EventBroadcast.publishEvent(1);
+        EventBroadcast.unRegister(receiver);
     }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(getApplicationContext(), "fuck", Toast.LENGTH_SHORT).show();
+        }
+    };
 
 }
