5dbe883ec7e95c7e5be5d05edf231f522ecd2ffa
yangzhongxu <zhongxu.yang@aliyun.com>
2016-03-07 14:36:59 +0800

    goon

--- src/main/java/yzx/study/frame2/MainActivity.java
+++ src/main/java/yzx/study/frame2/MainActivity.java
@@ -46,4 +46,11 @@ public class MainActivity extends AppCompatActivity {
 //        }
 //    };
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        GetSystemLog.stop();
+    }
+
 }
--- src/main/java/yzx/study/frame2/test/GetSystemLog.java
+++ src/main/java/yzx/study/frame2/test/GetSystemLog.java
@@ -7,22 +7,35 @@ import yzx.study.frame2.callback.GenericCallback;
 
 public class GetSystemLog {
 
+    private static boolean running = true;
+
     public static void getLog(final GenericCallback<String> callback) {
+        running = true;
         new Thread() {
             public void run() {
                 try {
                     Process process = Runtime.getRuntime().exec(new String[]{"logcat", "-d"});
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                     String[] clearArray = {"logcat", "-c"};
-                    String str = null;
+                    String str;
                     while ((str = bufferedReader.readLine()) != null) {
+                        if (!running)
+                            break;
                         Runtime.getRuntime().exec(clearArray);
                         callback.callback(str);
                     }
+                    bufferedReader.close();
+                    process.destroy();
                 } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }.start();
+    }
+
+
+    public static void stop() {
+        running = false;
     }
 
 }
