071be515ef863190c26e13f640151591603d3376
yangzhongxu <zhongxu.yang@aliyun.com>
2016-03-09 14:51:05 +0800

    goon

--- src/main/jni/____main____.cpp
+++ src/main/jni/____main____.cpp
@@ -1,4 +1,5 @@
 #include "myutil.h"
+#include "____main____.h"
 
 JNIEXPORT jstring Java_yzx_study_frame2_jnif_JNI_test(JNIEnv *env, jclass jc) {
     jstring jstr = yzx::get_jstring(env, "fuck");
---
+++ src/main/jni/____main____.h
@@ -0,0 +1,20 @@
+#include "jni.h"
+
+#ifndef _Included_yzx_study_frame2_jnif_JNI
+#define _Included_yzx_study_frame2_jnif_JNI
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+JNIEXPORT jstring Java_yzx_study_frame2_jnif_JNI_test(JNIEnv *, jclass);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
